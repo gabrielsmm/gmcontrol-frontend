eb29df7c1fc16943ac1c56d9a4f9ef270d83f137
gabrielsmm <gabrielsmm251@gmail.com>
2024-08-24 19:08:15 -0300

    feat: Estruturação inicial do desktop da membresia cristã

--- src/app/pages/membresia-crista/desktop-membresia-crista/desktop-membresia-crista.component.ts
+++ src/app/pages/membresia-crista/desktop-membresia-crista/desktop-membresia-crista.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-desktop-membresia-crista',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkWithHref,
+    NgbModule
+  ],
   templateUrl: './desktop-membresia-crista.component.html',
   styleUrl: './desktop-membresia-crista.component.scss'
 })
